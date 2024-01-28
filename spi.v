192dc123abc570dc13ad95877e813d1d08dce24c
Mishra-0709 <143344990+Mishra-0709@users.noreply.github.com>
2024-01-28 18:05:50 +0530

    Update spi.v

--- spi.v
+++ spi.v
@@ -47,3 +47,28 @@ assign spi_sclk=sclk;
 assign spi_data=MOSI;
 assign counter=count;
 endmodule
+
+\\\\\\\\\\\\\TESTBENCH\\\\\\\\\\\\
+
+`timescale 1ns/1ps
+module spi_tb();
+  reg clk,rst, [15:0]indata;
+  wire spi_cs_l,spi_sclk,spi_data, [4:0]counter;
+  
+  spi_blockflow dut(clk,rst,indata,spi_cs_l,spi_sclk,spi_data,counter);
+
+  initial
+    begin 
+    clk=0;
+      rst=1;
+      indata=0;
+    end
+  always #5clk=~clk;
+  initial
+    begin
+    #10 rst=1'b0;
+      #10 indata=16'h0412;
+      #335 indata=16'h4839;
+      #335 indata=16'habeb;
+    end
+endmodule
